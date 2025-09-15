695c555e2fb996998ce716c6f76749387e7d370b
Daniel <Daniel3204155185@gmail.com>
2025-09-14 23:13:47 -0500

    Refactor y estilizar componentes de gestión de infracciones: mejorar la estructura y estilos de formularios en 'anexar-multa' y 'generar-multa'.

--- src/app/feature/Inspectora/anexar-multa.component.ts
+++ src/app/feature/Inspectora/anexar-multa.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppTopbar } from '../topbar/topbar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-anexar-multa',
   templateUrl: './anexar-multa.component.html',
-  styleUrls: ['./generar-multa.component.scss'],
+  styleUrls: ['./anexar-multa.component.scss'],
   standalone: true,
   imports: [CommonModule, AppTopbar]
 })
-export class AnexarMultaComponent {}
+export class AnexarMultaComponent {
+  constructor(private router: Router) {}
+
+  volver() {
+    this.router.navigate(['/inspectora/generar-multa']);
+  }
+}
