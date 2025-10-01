9050abb67c5477188e7f77f1109e4a3cbadb9052
ingrid0208 <yulissamain.04@gmail.com>
2025-10-01 12:48:42 -0500

    merge

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -12,7 +12,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from '../../../app.routes';
-// import { authExpiredInterceptor } from '../interceptors/auth-expired.interceptor';
+import { authExpiredInterceptor } from '../interceptors/auth-expired.interceptor';
 
 // 👇 importa MessageService
 import { MessageService } from 'primeng/api';
